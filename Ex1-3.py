70afd057a6ac4e5e737eabf0c7c84f14fa21ae07
Alnaimi- <al.alnaimi@gmail.com>
2014-09-03 11:31:31 +0100

    Add exercise 1.3
    Face for background and glasses drawn on.

---
+++ Ex1-3.py
@@ -0,0 +1,39 @@
+import time
+from turtle import *
+
+screensize(1024,768)
+bgcolor("red")
+#time.sleep(3)
+clearscreen()
+#time.sleep(1)
+bgpic("background.gif")
+
+pensize(4)
+penup()
+goto(-10, 220)
+pendown()
+
+for i in range(2):
+	forward(150)
+	right(90)
+	forward(100)
+	right(90)
+
+penup()
+goto(-10, 170)
+pendown()
+right(180)
+forward(50)
+
+right(180)
+penup()
+goto(-210, 180)
+pendown()
+
+for j in range(2):
+        forward(150)
+        right(90)
+        forward(100)
+        right(90)
+
+
